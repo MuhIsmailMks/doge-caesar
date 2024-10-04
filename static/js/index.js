5928b1be1e5aac95b6002eff4611ce02a1c39716
muhammad ismail <muhismailmks@gmail.com>
2024-10-05 04:19:48 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -3,29 +3,24 @@ AOS.init({
     once: true
 });
  
- 
 
 // copy address
-const copyAddressAll = document.querySelectorAll('.copy-box');
+const copyAddress = document.querySelector('.copy-box');
     
-
+let text = document.querySelector('.copy-box__text'); 
+let btnText = text.textContent;
 let timeout;
 
-copyAddressAll.forEach(copyAddress => {
-    copyAddress.addEventListener('click', () => { 
-        let text = copyAddress.querySelector('.copy-box__text');
-        let btn = copyAddress.querySelector('.copy-box__text');
-        let btnText = btn.textContent;
-        navigator.clipboard.writeText(text.textContent).then(function () {
-            btn.textContent = 'Copied';
+copyAddress.addEventListener('click', () => { 
+    navigator.clipboard.writeText(text.textContent).then(function () {
+        text.textContent = 'Copied';
+
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            text.textContent = textText;
+        }, 2000);
+    }).catch(function (err) {
+        console.error('Failed to copy text: ', err);
+    });
     
-            clearTimeout(timeout);
-            timeout = setTimeout(function () {
-                btn.textContent = btnText;
-            }, 2000);
-        }).catch(function (err) {
-            console.error('Failed to copy text: ', err);
-        });
-        
-    })
 })
