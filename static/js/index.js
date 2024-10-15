bf7438d6dd404ad508d695972f3c5e4be44cc433
muhammad ismail <muhismailmks@gmail.com>
2024-10-16 07:12:19 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -3,61 +3,29 @@ AOS.init({
     once: true
 });
  
+ 
 
 // copy address
-const copyAddress = document.querySelector('.copy-box');
+const copyAddressAll = document.querySelectorAll('.copy-box');
     
-let text = document.querySelector('.copy-box__text'); 
-let btnText = text.textContent;
+
 let timeout;
 
-copyAddress.addEventListener('click', () => { 
-    navigator.clipboard.writeText(text.textContent).then(function () {
-        text.textContent = 'Copied';
-
-        clearTimeout(timeout);
-        timeout = setTimeout(function () {
-            text.textContent = textText;
-        }, 2000);
-    }).catch(function (err) {
-        console.error('Failed to copy text: ', err);
-    });
+copyAddressAll.forEach(copyAddress => {
+    copyAddress.addEventListener('click', () => { 
+        let text = copyAddress.querySelector('.copy-box__text');
+        let btn = copyAddress.querySelector('.copy-box__text');
+        let btnText = btn.textContent;
+        navigator.clipboard.writeText(text.textContent).then(function () {
+            btn.textContent = 'Copied';
     
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+                btn.textContent = btnText;
+            }, 2000);
+        }).catch(function (err) {
+            console.error('Failed to copy text: ', err);
+        });
+        
+    })
 })
-
-
-// video
-document.addEventListener("DOMContentLoaded", function() {
-    let videoServices = document.querySelectorAll('.video_service');
-
-    function playVideo(video) {
-        if (video.paused) {
-            video.play().catch(function(error) {
-                console.error("Video play was prevented:", error);
-            });
-        }
-    }
- 
-    for (let video of videoServices) { 
-        video.addEventListener('play', function() {
-            video.muted = true;
-        });
- 
-        video.addEventListener('loadeddata', function() {
-            playVideo(video);
-        });
- 
-        video.muted = true;
-        video.controls = false;
-    }
-});
- 
-document.addEventListener('DOMContentLoaded', function() {
-    let videos = document.querySelectorAll('.video_service');
-    videos.forEach(function(video) {
-        video.addEventListener('canplaythrough', function() {
-            video.play();
-        }, true);
-    });
-});
-
